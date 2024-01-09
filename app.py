f07792891a8ca7769fffa93a4d60eb43dd39e68b
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2024-01-09 18:11:47 +0100

    add app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+
+def encrypt():
+    pass
+
+
+def decrypt():
+    pass
+
+
+app = Flask(__name__)
+
+
+@app.route("/home")
+pass
+
+
+if __name__ == "__main__":
+    app.run("localhost", 8080, debug=True)
