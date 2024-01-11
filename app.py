aa615912c23c272e21eb59c5a1e3f7a07d304190
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2024-01-11 10:36:39 +0100

    pierwszy kod do szyfrowania, zaraz testujemy

--- app.py
+++ app.py
@@ -2,19 +2,28 @@ from flask import Flask
 from flask import render_template 
 
 
-app = Flask(__name__)
-
-
 enc = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'ź', 'ż']
 dec = ['⁅', '⁆', '<', '>', '⌈', '⌉', '«', '»', '⌊', '⌋', '⟨', '⟩', '⟦', '⟧', '⟪', '⟫', '⟬', '⟭', '‡', '¶', '⁋', '⁕', '‣', '⁌', '⁜', '※', '♪', '✕', '⌀', '⏕', '⏔', '⁒', '‽', '⸘']
 
 
-def encrypt():
-    pass
+key = int(input())
+
+def encrypt(text):
+    text = ""
+    key_for_enc = (key // (key * 8))**2 
+    for i in text:
+        if (ord(i) - key_for_enc < 98):
+            text += chr(ord(i) - key_for_enc + key)
+        else:
+            text += chr(ord(text) - key_for_enc)
+    return text
 
 
 def decrypt():
     pass
+
+
+app = Flask(__name__)
 
 
 @app.route("/")
