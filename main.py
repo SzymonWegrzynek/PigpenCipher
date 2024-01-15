8084e33f7600f87091763e974e2b3eb062741cf8
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2024-01-15 14:22:11 +0100

    całkiem calkiem działa

--- main.py
+++ main.py
@@ -2,25 +2,25 @@ enc = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 
 dec = ['⁅', '⁆', '<', '>', '⌈', '⌉', '«', '»', '⌊', '⌋', '⟨', '⟩', '⟦', '⟧', '⟪', '⟫', '⟬', '⟭', '‡', '¶', '⁋', '⁕', '‣', '⁌', '⁜', '※', '♪', '✕', '⌀', '⏕', '⏔', '⁒', '‽', '⸘']
 
 
-def encrypt(text):
-    text = str(text.lower())
-    pigpen_text = ""
-    # key_for_enc = (key // (key * 8))**2 
-    for i in text:
-        if i in dec:
-            pigpen_text += enc[dec.index(i)]
-        else:
-            pigpen_text += i
-    return pigpen_text
-
-
-def decrypt(pigpen_text):
+def encrypt(pigpen_text, key):
     pigpen_text = str(pigpen_text.lower())
     text = ""
-    # key_for_enc = (key // (key * 8))**2 
     for i in pigpen_text:
         if i in enc:
-            text += dec[enc.index(i)]
+            index = (enc.index(i) - key) % len(enc)
+            text += dec[index]
         else:
             text += i
     return text
+
+
+def decrypt(text, key):
+    text = str(text.lower())
+    pigpen_text = ""
+    for i in text:
+        if i in dec:
+            index = (dec.index(i) + key) % len(dec)
+            pigpen_text += enc[index]
+        else:
+            pigpen_text += i
+    return pigpen_text
