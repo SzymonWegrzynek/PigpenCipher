84cbdfc9f5ebe99eef9bda000bc34d552bd8920a
Szymon Węgrzynek <u105_szyweg_waw@technischools.com>
2024-01-18 14:30:03 +0100

    commit

--- src/app.py
+++ src/app.py
@@ -1,13 +1,32 @@
 from flask import Flask
-from flask import render_template 
+from flask import request
+from flask import render_template
+from pigpen_cipher import encrypt
+from pigpen_cipher import decrypt
 
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def page():
+def home():
     return render_template('index.html')
+
+
+@app.post("/enc")
+def enc():
+    key = request.form["key"]
+    text = request.form["text"]
+    pigpen_text = encrypt(text)
+    return pigpen_text
+
+
+@app.post("/dec")
+def dec():
+    key = request.form["key"]
+    pigpen_text = request.form["pigpen_text"]
+    text = decrypt(pigpen_text)
+    return text
 
 
 if __name__ == "__main__":
